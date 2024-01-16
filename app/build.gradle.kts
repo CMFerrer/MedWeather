c3140a186da59ae105ef9fd2884c5f3fa50c932e
CMFerrer <70974749+CMFerrer@users.noreply.github.com>
2024-01-16 11:37:13 -0500

    Add icon app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -102,7 +102,7 @@ dependencies {
 
     // Location
     implementation("com.google.android.gms:play-services-location:21.0.1")
-
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/chiksmedina/medweather/search/ui/SearchScreen.kt
+++ app/src/main/java/com/chiksmedina/medweather/search/ui/SearchScreen.kt
@@ -1,5 +1,7 @@
 package com.chiksmedina.medweather.search.ui
 
+import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -23,10 +25,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import com.chiksmedina.solar.OutlineSolar
@@ -34,6 +38,10 @@ import com.chiksmedina.solar.outline.Arrows
 import com.chiksmedina.solar.outline.Search
 import com.chiksmedina.solar.outline.arrows.AltArrowLeft
 import com.chiksmedina.solar.outline.search.MinimalisticMagnifer
+import com.google.android.gms.location.LocationServices
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 @Composable
 fun SearchWrapper(
@@ -73,8 +81,17 @@ fun SearchScreen(
 
 }
 
+@SuppressLint("MissingPermission")
 @Composable
 fun NewSearch(onBackPress: () -> Unit, search: (String) -> Unit) {
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
+    val locationClient = remember {
+        LocationServices.getFusedLocationProviderClient(context)
+    }
+    var locationInfo by remember {
+        mutableStateOf("")
+    }
     SearchWrapper(onBackPress = onBackPress, search = search) {
         Box(
             modifier = Modifier
@@ -83,7 +100,17 @@ fun NewSearch(onBackPress: () -> Unit, search: (String) -> Unit) {
             contentAlignment = Alignment.Center
         ) {
             Button(
-                onClick = { /*TODO*/ },
+                onClick = {
+                    scope.launch(Dispatchers.IO) {
+                        val result = locationClient.lastLocation.await()
+                        locationInfo = if (result == null) {
+                            "No last known location. Try fetching the current location first"
+                        } else {
+                            "Current location is \n" + "lat : ${result.latitude}\n" +
+                                    "long : ${result.longitude}\n" + "fetched at ${System.currentTimeMillis()}"
+                        }
+                        Log.d("Location", locationInfo)
+                    } },
                 modifier = Modifier.fillMaxWidth(),
                 shape = MaterialTheme.shapes.medium
             ) {
